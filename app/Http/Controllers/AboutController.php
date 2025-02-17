bf13b198ec2fb5b93479ddf5a5fa8b5ef6c6bbe2
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2025-02-17 15:00:13 +0700

    Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return "Nama: Agita Putri Salsabila Aji dan NIM: 2341760092";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -55,5 +55,40 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
+
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
     
-    
+use App\Http\Controllers\PageController;
+
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+// Route untuk halaman utama
+Route::get('/', HomeController::class);
+
+// Route untuk halaman about
+Route::get('/about', AboutController::class);
+
+// Route untuk halaman artikel
+Route::get('/articles/{id}', ArticleController::class);
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
