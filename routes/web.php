162511980c6155fedecb5b47028476b69571b2cf
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2025-02-17 13:39:30 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/hello', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+ 
+
+Route::get('/world', function () {
+    return 'World';
+     
+});
+
+Route::get('/world', function () {
+    return 'Selamat Datang';
+     
+});
+
+Route::get('/about', function () {
+    return '2341760092, Agita Putri Salsabila Aji';  
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+    
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID $id";
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+    
+    
